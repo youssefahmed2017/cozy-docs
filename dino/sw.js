bf31b339d169c4a0105818236f21cc9f8f10b284
youssefdev <youssef.ahmed.29062017@gmail.com>
2026-06-11 14:12:57 +0300

    Add files via upload

---
+++ dino/sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = "cozykit-v1";
+
+const FILES_TO_CACHE = [
+    "/offline.html",
+    "/dino.html"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    if (event.request.mode === "navigate") {
+        event.respondWith(
+            fetch(event.request)
+                .catch(() => caches.match("/offline.html"))
+        );
+    }
+});
