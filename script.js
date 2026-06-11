1c8f39efd98d04eb0d59aacec88c897184d7e459
youssefdev <youssef.ahmed.29062017@gmail.com>
2026-06-11 14:24:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("dino/sw.js")
+    navigator.serviceWorker.register("/sw.js")
         .then(() => console.log("SW registered"))
         .catch(err => console.error(err));
 }
