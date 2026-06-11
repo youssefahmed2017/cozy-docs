9ec12e735c3325073f99d74080e3dca3539d764f
youssefdev <youssef.ahmed.29062017@gmail.com>
2026-06-11 15:14:27 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
-const CACHE_NAME = "cozykit-v2";
+const CACHE_NAME = "cozykit-v3";
 
 const FILES_TO_CACHE = [
-    "dino/offline.html",
-    "dino/dino.html"
+    "dino/offline.html"
 ];
 
 self.addEventListener("install", event => {
@@ -15,14 +14,6 @@ self.addEventListener("install", event => {
 self.addEventListener("fetch", event => {
     event.respondWith(
         fetch(event.request).catch(() => {
-
-            // If the iframe requests the game,
-            // give it the cached game instead of offline.html
-            if (event.request.url.includes("dino/dino.html")) {
-                return caches.match("dino/dino.html");
-            }
-
-            // Main page navigations get the offline page
             if (event.request.mode === "navigate") {
                 return caches.match("dino/offline.html");
             }
